154abcc4a1425f47ea1a6c4d5115a184b39bc099
bunmidavies <bunzdavies@gmail.com>
2017-10-21 11:46:34 +0100

    initial

--- docs/main.js
+++ docs/main.js
@@ -1,13 +1,23 @@
 function _(x){
     return document.getElementById(x);
 }
+
 function login(){
     user = _("user").value;
     pass = _("pw").value;
+    $.get('logins.txt', function(data) {
+        data = data.split("\n");
+        if(data[0] == "ey"){
+            console.log("ree");
+        }
+     }, 'text');
     if(user == "ree" && pass=="nig"){
         console.log("logged in.")
+        _("details").innerHTML = "You have 30 clout tokens.";
+        
     }
     else{
         console.log("failed.")
+        _("details").innerHTML = "";
     }
 }
